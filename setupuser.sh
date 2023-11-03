0c73c96cad2f54505c306b6dbffa5ceb58d11383
Russell Bate <russell.bate@cern.ch>
2023-11-03 13:36:13 +0100

    Added sym link set up and vanillification.

---
+++ setupuser.sh
@@ -0,0 +1,16 @@
+echo -e "\nSetting up sym links\n"
+
+cd ~
+
+echo "ln -s -T dotfiles/.bashrc .bashrc"
+ln -s -T dotfiles/.bashrc .bashrc
+echo "ln -s -T dotfiles/.tmux.conf .tmux.conf"
+ln -s -T dotfiles/.tmux.conf .tmux.conf
+echo "ln -s -T dotfiles/.vimrc .vimrc"
+ln -s -T dotfiles/.vimrc .vimrc
+echo "ln -s -T dotfiles/.screenrc .screenrc"
+ln -s -T dotfiles/.screenrc .screenrc
+echo "ln -s -T dotfiles/.gitconfig .gitconfig"
+ln -s -T dotfiles/.gitconfig .gitconfig
+
+echo -e "\n\tdone!"
