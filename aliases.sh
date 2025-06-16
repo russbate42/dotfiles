90603addf74c83ad9e631dfd495347546e6cc60e
Russell Bate <r.bate@hotmail.com>
2025-06-16 14:31:19 +0200

    Changed lxplus alias to a function accepting a node.

--- aliases.sh
+++ aliases.sh
@@ -2,7 +2,6 @@ alias kitty='kitten'
 alias icat='kitty kitten icat'
 
 alias nv=~/sandbox/nvim.appimage
-alias lx='ssh rbate@lxplus.cern.ch'
 alias triumf='ssh russbate@triumf-ml1.phas.ubc.ca'
 alias cedar='echo "Not set up yet."'
 alias triumf_t3='echo "Not set up yet."'
@@ -12,6 +11,7 @@ alias tmuxu='tmux new-session -s R-Ubuntu'
 alias lsd='ls -ld */'
 alias lsah='ls -lah'
 alias lsda='ls -lda */'
+alias lssh='ls -la | grep sh'
 alias lsf='find . -maxdepth 1 -type f'
 alias gst='git status'
 alias lspy='ls -l *.py'
@@ -31,3 +31,16 @@ function checkmem() {
 		du -h ./$1 | sort -h
     fi
 }
+
+function thing(){
+    printf "in thing function\n"
+    local node=$1
+    ssh "rbate@lxplus${node}.cern.ch"
+    # exit_status=!?
+    # if [[ "$exit_status" == "" ]]; then
+    #
+}
+function lx(){
+    ssh "rbate@lxplus${1}.cern.ch"
+}
+
