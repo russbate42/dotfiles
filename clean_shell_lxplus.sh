8a842fb7ac0e1fe03de8bb3344ae9ef36597ae28
Russell Bate <russell.bate@cern.ch>
2026-03-06 05:56:14 +0100

    Updated cleanshell script to have basic env variables.

--- clean_shell_lxplus.sh
+++ clean_shell_lxplus.sh
@@ -1,1 +1,7 @@
-env -i bash --noprofile --rcfile ~/dotfiles/.bashrc_simple
+# env -i bash --noprofile --rcfile ~/dotfiles/.bashrc_simple
+env -i\
+    HOME="$HOME" \
+    USER="$USER" \
+    PATH="$PATH" \
+    TERM="$TERM" \
+    bash --noprofile --rcfile ~/dotfiles/.bashrc_simple
